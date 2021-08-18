ca575e62b0dc98d158e82e40536a790020d375bd
Ilya Medvedev <imedvedev@readymag.com>
2021-08-18 11:50:39 +0300

    test: 💍 test for generating scripts

---
+++ __test__/module.test.ts
@@ -0,0 +1,32 @@
+import * as ts from "typescript";
+import { start } from "../src/module";
+
+beforeEach(() => {
+  window.ts = ts;
+
+  // Set up our document body
+  document.head.innerHTML = `
+    <script type="ts-module" resolver="skypack">
+      import * as lodash from "lodash";
+      import * as React from "react";
+      import * as ReactDOM from "react-dom";
+
+      function App() {
+        return <div>Hello world</div>
+      }
+
+      ReactDOM.render(<App />, document.getElementById('container'));
+    </script>`;
+});
+
+describe("generate script[type=module] from script[ts-module]", () => {
+  it("generate right code", () => {
+    start();
+
+    const module = document.querySelector('script[type="module"]');
+    expect(module).toBeInTheDocument();
+
+    const importmap = document.querySelector('script[type="importmap"]');
+    expect(importmap).toBeInTheDocument();
+  });
+});
