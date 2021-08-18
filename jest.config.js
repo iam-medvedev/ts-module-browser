448df3ceb08c242abdd89ece13c29f8d37d9fd86
Ilya Medvedev <imedvedev@readymag.com>
2021-08-18 18:19:13 +0300

    test: 💍 tsconfig for test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.test.json",
+    },
+  },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
