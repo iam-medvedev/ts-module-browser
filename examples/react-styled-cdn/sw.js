8e62ef859edeea5ceec56b014562dd1a7ad01426
iam-medvedev <iam-medvedev@users.noreply.github.com>
2021-08-21 13:15:52 +0000

    Deploying to gh-pages from @ iam-medvedev/ts-module-browser@db5d45e2dd385e00a5d7f9465989efa67f37c935 🚀

---
+++ examples/react-styled-cdn/sw.js
@@ -0,0 +1,9 @@
+// Load typescript (you can use any version you want)
+self.importScripts(
+  "https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescriptServices.js"
+);
+
+// Load sw code from CDN
+self.importScripts(
+  "https://cdn.jsdelivr.net/npm/ts-module-browser@latest/dist/sw.js"
+);
